4afa92fd87e74a75d69bb63b762939081c748021
505 <505@academicos.cesde.edu.co>
2020-02-10 20:48:40 -0500

    ejemplo node

--- ejercicio1/variable.js
+++ ejercicio1/variable.js
@@ -1,1 +1,14 @@
 let costo = 5 ;
+let comida = "";
+if (costo > 3 ){
+let comida = "galletas";
+if(costo>4){
+    let paseo = "jardin";
+}
+}
+console.log("la comida : "+ comida);
+
+for (var i =0; i<5; i++){
+
+}
+console.log (i);
